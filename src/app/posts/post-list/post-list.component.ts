093d487caa61b0053b3dcf3e2796065043b5c2d2
Sai-Pyneni <spyneni@students.kennesaw.edu>
2019-11-03 14:06:50 -0600

    first commit
    New commit for new project

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+ /*  posts = [
+    {title: 'First Post', content: 'this is a first post comment'},
+    {title: 'Second Post', content: 'this is a second post comment'},
+  ]; */
+ @Input() posts = [];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
