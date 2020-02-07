cd78339062ec850de0a66718269c846203d0e4e9
sp713s <sp713s@att.com>
2020-02-07 14:27:27 -0600

    Reorganized structure of backend routes

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
-const Post = require("./models/posts")
+
+const postsRoutes = require("./routes/posts");
 
 mongoose.connect('mongodb+srv://sp713s:RlVRTZ2OWpih1SlU@cluster0-b9ks5.mongodb.net/test?retryWrites=true&w=majority',{
   useUnifiedTopology: true,
@@ -33,65 +34,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post ("/api/posts", (req,res,next)=>{
-const posts = new Post({
-  title: req.body.title,
-  content: req.body.content
-});
-posts.save().then(result => {
-  res.status(201).json({
-    message: "Post added successfully",
-    postId: result._id  
-});
-console.log(posts);
+app.use("/api/posts",postsRoutes);
 
-});
-
-});
-
-app.get ( "/api/posts" ,(req, res, next) => {
-  Post.find().then(documents => {
-    console.log(documents);
-    res.status(200).json({
-      message: "Posts fetched succesfully!",
-      posts: documents
-    });
-  });
-  
-  
-});
-
-app.put("/api/posts/:id", (req,res,next)=>{
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({_id: req.params.id}, post).then(result =>{
-    console.log(result);
-    res.status(200).json({message: "Update Successful!"});
-
-  })
-
-});
-
-app.get("/api/posts/:id", (req,res,next)=>{
-Post.findById(req.params.id).then(post=> {
-  if(post)
-  {
-    res.status(200).json(post);
-
-  }
-  else
-  res.status(404).json({message: "Post not found!"});
-});
-  
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted!" });
-  });
-});
   module.exports = app;
