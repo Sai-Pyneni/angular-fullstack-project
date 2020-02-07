f9fa0e945e24d4c863875ad037bcc6d50bbfc909
sp713s <sp713s@att.com>
2020-02-07 14:04:10 -0600

    Post Updated on Server

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,7 +15,7 @@ enteredTitle = '';
   enteredContent = '';
   private mode = 'create';
   private postId: string;
-  public post: Post;
+  post: Post;
 
 
  
@@ -29,7 +29,9 @@ enteredTitle = '';
       if (paramMap.has('postId')){
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
-        this.post = this.postsService.getPost(this.postId);
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.post= {id: postData._id, title: postData.title, content: postData.content};
+        });
       }
       else{ this.mode = 'create';
       this.postId = null;
